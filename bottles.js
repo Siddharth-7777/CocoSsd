aca92f933c14547c515fec1921e7daf23d183baa
Siddharth-7777 <77883380+Siddharth-7777@users.noreply.github.com>
2021-10-05 17:35:32 +0530

    Add files via upload

---
+++ bottles.js
@@ -0,0 +1,45 @@
+img = ""
+stat = ""
+objects = []
+
+
+function preload() {
+    img = loadImage("bottles.jpg")
+}
+
+function setup() {
+    canvas = createCanvas(800,450)
+    canvas.center()
+    CocoSsd = ml5.objectDetector("cocossd", cheat)
+    document.getElementById("status").innerHTML = "Status: Detecting Objects"
+}
+
+function cheat() {
+    console.log("Cheat Operation Successful! Now time to hack into this computer/phone/laptop/tablet.")
+    stat = true;
+    CocoSsd.detect(img, gotResult)
+}
+
+function draw() {
+    image(img, 0, 0, 800, 450)
+    if (stat != "") {
+       for (var i = 0; i < objects.length; i++) {
+           document.getElementById("status").innerHTML = "Status: Object Detected"
+           percent = Math.floor(objects[i].confidence * 100)
+           document.getElementById("number").innerHTML = "There are 13 big objects and " + objects.length + " were detected" 
+           text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y)
+           noFill()
+           stroke("blue")
+           rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+       }
+    }
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error)
+    } else {
+        console.log(results)
+        objects = results;
+    }
+}
